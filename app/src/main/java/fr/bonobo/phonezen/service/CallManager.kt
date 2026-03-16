2f863b67a5e5a09ddf641a92355ce82108054cd5
James007 <souffly007@gmail.com>
2026-03-16 17:18:22 +0100

    *.iml .gradle /local.properties /.idea/caches /.idea/libraries /.idea/modules.xml /.idea/workspace.xml /.idea/navEditor.xml /.idea/assetWizardSettings.xml .DS_Store /build /captures .externalNativeBuild .cxx local.properties

--- app/src/main/java/fr/bonobo/phonezen/service/CallManager.kt
+++ app/src/main/java/fr/bonobo/phonezen/service/CallManager.kt
@@ -1,3 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+// Copyright (C) 2025-2026 Franck R-F (souffly007)
+// This file is part of PhoneZen.
+//
+// PhoneZen is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License.
 package fr.bonobo.phonezen.service
 
 import android.media.AudioManager
