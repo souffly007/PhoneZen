3b49aa0729cfb63184faebb76df09cf5bfe272bf
souffly007 <133746295+souffly007@users.noreply.github.com>
2026-08-23 15:49:39 +0200

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -130,7 +130,4 @@ dependencies {
     // JavaMail pour Android (IMAP)
     implementation("com.sun.mail:android-mail:1.6.7")
     implementation("com.sun.mail:android-activation:1.6.7")
-
-    // ✅ Google Play Services : UNIQUEMENT pour le flavor "google"
-    googleImplementation("com.google.android.gms:play-services-location:21.0.1")
 }
